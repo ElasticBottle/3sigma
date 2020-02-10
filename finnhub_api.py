105473965dab4d4145c8888fc8f769af973aa055
Winston <winstonyeo99@yahoo.com>
2020-02-10 11:01:26 +0800

    Fix datetime conversion for finnhub api

--- finnhub_api.py
+++ finnhub_api.py
@@ -1,5 +1,7 @@
 import requests
 import api_keys as key
+import datetime
+import time
 
 
 class Finnhub:
@@ -12,7 +14,7 @@ class Finnhub:
         "hourly": 4,
         "daily": 5,
     }
-    resolutions = [1, 5, 15, 30, 60, "D"]
+    resolutions = ["1", "5", "15", "30", "60", "D"]
 
     def __init__(self):
         super().__init__()
@@ -32,15 +34,16 @@ class Finnhub:
                     - "daily
             from_date (datetime): Provides the start of the timeline to get data for.
                 Format should be datetime.datetime(year, month, day, hour, minute, second)
-                Remeber to import datetime in the file you're using first
+                Remember to import datetime in the file you're using first
             to_date(datetime): Provides the end of the timeline to get data for.
                 Format same as from_date.
         """
-        from_date = from_date.strftime("%s")
-        to_date = to_date.strftime("%s")
+        from_date = str(int(datetime.datetime.timestamp(from_date)))
+        to_date = str(int(datetime.datetime.timestamp(to_date)))
+        print("from Date:", from_date, "to_date: ", to_date)
         r = requests.get(
             "https://finnhub.io/api/v1/crypto/candle?symbol=BINANCE:BTCUSDT&resolution="
-            + self.resolutions[resolution]
+            + self.resolutions[self.values[resolution]]
             + "&from="
             + from_date
             + "&to="
